b09be64deb17c5b92b25124bfe2ec625fbb64d72
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2019-09-05 19:54:15 +0700

    - Initial commit

---
+++ heroku.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const ParallelRequest = require('parallel-http-request');
+var request = new ParallelRequest();
+var timestamp = new Date().toISOString().replace('T',' ').replace('Z','').substr(0,19);
+
+app.get('/', (req, res) => res.send(new Date().toISOString().replace('T',' ').replace('Z','').substr(0,19)+' : Hello World!'));
+
+
+/**
+ * Configuration
+ */
+// const port = 3000;              // dev mode
+const port = process.env.PORT;  //heroku
+const awake = 600000;            // awake in miliseconds
+const website = [
+    'https://private-awaker.herokuapp.com/',
+    'https://thawing-escarpment-54169.herokuapp.com/'
+];
+
+
+/**
+ * RUN
+ */
+app.listen(port, () => {
+    console.log('Started at '+timestamp);
+    console.log(`Awaker is listening on port ${port}!`);
+    console.log('Will awake for every '+awake+'ms.');
+});
+
+setInterval(function(){
+    request.clean();
+    for(let i in website) {
+        request.add(website[i]);
+    }
+    request.send(function(response){
+        response.forEach(function(v){ delete v.method;delete v.body; });
+        console.log(response);
+    })
+},awake);
